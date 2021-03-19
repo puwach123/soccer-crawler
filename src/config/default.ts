99ba295b148256950f8ebd7561a8285783ea3fc4
wcpun <puwach123@gmail.com>
2021-03-20 04:49:07 +0800

    add config

---
+++ src/config/default.ts
@@ -0,0 +1,9 @@
+const config: any = {};
+config.match.ipath =
+  'C:\\Users\\admin\\Desktop\\scraper\\src\\data\\teams.json';
+config.match.opath =
+  'C:\\Users\\admin\\Desktop\\scraper\\src\\data\\matchs.json';
+config.team.ipath =
+  'C:\\Users\\admin\\Desktop\\scraper\\src\\data\\leagues.json';
+config.team.opath = 'C:\\Users\\admin\\Desktop\\scraper\\src\\data\\teams.json';
+export default config;
